6c0574282aceee1088b4d6a9f843170dcc6f60a2
Sekou Dosso <sekou.dosso82@gmail.com>
2019-12-31 16:40:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,9 +9,15 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function addToCart(item)
+{
+ 
+
+ var newObject = {itemName: item,itemPrice: Math.floor(Math.random() * (100 - 1 + 1)) + 1}
+ cart.push(newObject);
+ return `${item} has been added to your cart.`;
 }
+
 
 function viewCart() {
   // write your code here
